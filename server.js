5e1c8e05e6d01fd6e228cc0c6354d5d5cc8202ca
Kira-S <41986076+Kira-S@users.noreply.github.com>
2019-10-19 17:40:06 +0300

    updating database data

--- server.js
+++ server.js
@@ -12,10 +12,8 @@ const image = require('./controllers/image')
 const db = knex({
     client: 'pg',
     connection: {
-        host: 'postgresql-clear-28251',
-        user: 'postgres',
-        password: '123',
-        database: 'smart-brain'
+        host: process.env.DATABASE_URL,
+        ssl: true
     }
 });
 
